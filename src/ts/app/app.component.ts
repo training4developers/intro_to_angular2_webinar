3bc0b9019e88d813cba942d74e075c48a69c8d3a
Eric W. Greene <eric@ericwgreene.com>
2017-02-16 12:52:47 -0500

    added code demos

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,8 +2,29 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "color-tool",
-    template: `<h1>{{header}}</h1>`,
+    template: `
+        <h1>{{header}}</h1>
+        <item-list [items]="colors"></item-list>
+        <form>
+            <div>
+                <label for="new-color-input">New Color</label>
+                <input type="text" id="new-color-input" name="newColorInput" [(ngModel)]="newColor">
+            </div>
+            <button type="button" (click)="addColor()">Add Color</button>
+        </form>
+    `,
 })
 export class AppComponent {
-    public header: string = "Hello World!";
+
+    public header: string = "Color Tool";
+    public newColor: string = "";
+
+    public colors: string[] = [
+        "red", "white", "green", "blue", "yellow",
+        "black",
+    ];
+
+    public addColor() {
+        this.colors.push(this.newColor);
+    }
 }
--- src/ts/app/app.module.ts
+++ src/ts/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { ItemList } from "./components/item-list.component";
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent, ItemList ],
     bootstrap: [ AppComponent ],
 })
 export class AppModule { }
